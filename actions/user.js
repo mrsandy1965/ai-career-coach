08f455c412c64eaa75d5843b9cea97814d8a04a9
SANDESH LENDVE <123884782+mrsandy1965@users.noreply.github.com>
2025-09-20 10:35:37 +0000

    done

---
+++ actions/user.js
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { generateAIInsights } from "./dashboard";
+
+export async function updateUser(data) {
+  // ...implementation...
+}
+
+export async function getUserOnboardingStatus() {
+  // ...implementation...
+}
